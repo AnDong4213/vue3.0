4523a6d131186ade355d21c33a2eb453578fb29c
赵安东 <zhaoandong4213@sina.com>
2021-03-17 14:16:18 +0800

    init vue3

---
+++ vue3-basic/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <img alt="Vue logo"
+       src="./assets/logo.png">
+  <h1>{{count}}</h1>
+  <h1>{{double}}</h1>
+  <button @click="increase">+1</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from "vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    // count就是一个响应式的对象，可以监测到改变
+    const count = ref(0);
+    const double = computed(() => {
+      return count.value * 2;
+    });
+    const increase = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      increase,
+      double
+    };
+  },
+  components: {}
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
