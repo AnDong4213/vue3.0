1fd86a9e85250db12313074806049baebfe74949
赵安东 <zhaoandong4213@sina.com>
2022-01-27 11:40:25 +0800

    feat: v3

---
+++ vue-no-ts/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png" alt="" />
+    <HelloWorld msg="Vue" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped lang='less'>
+.home {
+  color: red;
+}
+</style>
