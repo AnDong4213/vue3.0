bcb3aece748aa323a9c79b0fe42b84403b3fec74
赵安东 <zhaoandong4213@sina.com>
2022-02-24 17:32:10 +0800

    feat: 0204

--- interview/js3.js
+++ interview/js3.js
@@ -1,25 +1,31 @@
-/* async function async1() {
-  console.log("async1 start"); // 1
+async function async1() {
+  console.log("1"); 
   await async2();
-  console.log("async1 end"); // 7
+  console.log("2"); 
+  Promise.resolve('9').then(res => console.log(res))
 }
-async function async2() {
-  console.log("async2"); // 2
-}
-async1();
-new Promise(function (resolve) {
-  console.log("promise1"); // 3
-  resolve();
-  console.log("promise1 end"); // 4
-}).then(function () {
-  console.log("promise2"); // 8
-});
-setTimeout(function () {
-  console.log("settimeout"); // 9
-});
-console.log("script start"); // 5
 
-console.log("script end"); // 6 */
+async function async2() {
+  console.log("3"); 
+}
+
+async1();
+
+new Promise(function (resolve) {
+  console.log("4"); 
+  resolve();
+  console.log("5");
+}).then(function () {
+  console.log("6");
+});
+
+setTimeout(function () {
+  console.log("7"); 
+});
+
+console.log("8"); 
+
+// 1 3 4 5 8  2 6 9  7
 
 // var age = 8;
 
