1d9ac4a3cc8d415c7ec7cdd85d25b2252ccc9c1e
赵安东 <zhaoandong4213@sina.com>
2022-03-13 18:28:30 +0800

    feat: 播放模式

--- yinyue/src/components/player/player.vue
+++ yinyue/src/components/player/player.vue
@@ -13,43 +13,191 @@
         <h1 class="title">{{currentSong.name}}</h1>
         <h2 class="subtitle">{{currentSong.singer}}</h2>
       </div>
+      <div class="bottom">
+        <div class="dot-wrapper">
+          <span class="dot"></span>
+          <span class="dot"></span>
+        </div>
+        <div class="progress-wrapper">
+          <span class="time time-l"></span>
+          <div class="progress-bar-wrapper">
+            <progress-bar ref="barRef"></progress-bar>
+          </div>
+          <span class="time time-r"></span>
+        </div>
+        <div class="operators">
+          <div class="icon i-left">
+            <i @click="changeMode"
+               :class="modeIcon"></i>
+          </div>
+          <div class="icon i-left"
+               :class="disableCls">
+            <i @click="prev"
+               class="icon-prev"></i>
+          </div>
+          <div class="icon i-center"
+               :class="disableCls">
+            <i @click="togglePlay"
+               :class="playIcon"></i>
+          </div>
+          <div class="icon i-right"
+               :class="disableCls">
+            <i @click="next"
+               class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i @click="toggleFavorite(currentSong)"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <audio ref="audioRef"></audio>
+    <!--oncanplay 当媒介能够开始播放但可能因缓冲而需要停止时运行脚本 -->
+    <audio ref="audioRef"
+           @pause="pause"
+           @canplay="ready"
+           @error="error"></audio>
   </div>
 </template>
 
 <script>
 import { computed, watch, ref } from 'vue'
 import { useStore } from 'vuex'
+import useMode from './use-mode'
 import './player.scss'
 
 export default {
   setup() {
+    // data
     const audioRef = ref(null)
+    const songReady = ref(false)
+    // const currentTime = ref(0)
 
     // vuex
     const store = useStore()
     const fullScreen = computed(() => store.state.fullScreen)
     const currentSong = computed(() => store.getters.currentSong)
+    const playing = computed(() => store.state.playing)
+    const currentIndex = computed(() => store.state.currentIndex)
 
+    // hooks
+    const { modeIcon, changeMode } = useMode()
+
+    // computed
+    const playlist = computed(() => store.state.playlist)
+    const playIcon = computed(() => {
+      return playing.value ? 'icon-pause' : 'icon-play'
+    })
+    const disableCls = computed(() => {
+      return songReady.value ? '' : 'disable'
+    })
+
+    // watch
     watch(currentSong, (newSong) => {
       if (!newSong.id || !newSong.url) {
         return
       }
+      songReady.value = false
       const audioEl = audioRef.value
       audioEl.src = newSong.url
       audioEl.play()
     })
+    watch(playing, (newPlaying) => {
+      if (!songReady.value) {
+        return
+      }
+      const audioEl = audioRef.value
+      if (newPlaying) {
+        audioEl.play()
+      } else {
+        audioEl.pause()
+      }
+    })
 
+    // methods
     function goBack() {
       store.commit('setFullScreen', false)
+    }
+
+    function prev() {
+      const list = playlist.value
+      if (!songReady.value || !list.length) {
+        return
+      }
+      if (list.length === 1) {
+        loop()
+      } else {
+        let index = currentIndex.value - 1
+        if (index === -1) {
+          index = list.length - 1
+        }
+        store.commit('setCurrentIndex', index)
+      }
+    }
+
+    function togglePlay() {
+      if (!songReady.value) {
+        return
+      }
+      store.commit('setPlayingState', !playing.value)
+    }
+
+    function next() {
+      const list = playlist.value
+      if (!songReady.value || !list.length) {
+        return
+      }
+
+      if (list.length === 1) {
+        loop()
+      } else {
+        let index = currentIndex.value + 1
+        if (index === list.length) {
+          index = 0
+        }
+        store.commit('setCurrentIndex', index)
+      }
+    }
+
+    function loop() {
+      const audioEl = audioRef.value
+      audioEl.currentTime = 0
+      audioEl.play()
+      store.commit('setPlayingState', true)
+    }
+
+    function toggleFavorite() { }
+    function pause() {
+      store.commit('setPlayingState', false)
+    }
+
+    function ready() {
+      if (songReady.value) {
+        return
+      }
+      songReady.value = true
+    }
+
+    function error() {
+      songReady.value = true
     }
 
     return {
       audioRef,
       fullScreen,
       currentSong,
-      goBack
+      playIcon,
+      disableCls,
+      goBack,
+      prev,
+      togglePlay,
+      next,
+      toggleFavorite,
+      pause,
+      ready,
+      error,
+      // mode
+      modeIcon,
+      changeMode
     }
   }
 }
